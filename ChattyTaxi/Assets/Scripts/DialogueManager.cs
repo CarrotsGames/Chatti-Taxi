ffb96862fec4938d69c68285e8f97362a5b371d5
Gabriel P <gaberofl42@gmail.com>
2019-04-01 13:33:13 +1100

    bug fixes

--- ChattyTaxi/Assets/Scripts/DialogueManager.cs
+++ ChattyTaxi/Assets/Scripts/DialogueManager.cs
@@ -7,6 +7,7 @@ public class DialogueManager : MonoBehaviour {
 
     public float TipAmount;
     public float Tip;
+    public GameObject TextBox;
     public GameObject[] Events;
     private int EventSteps = 0;
     public int Val;
@@ -37,6 +38,14 @@ public class DialogueManager : MonoBehaviour {
                     // Progress is added by 3 to continue through the list
                     TestText.text = CustomerDialogueNegative[0 + Progress];
                 }
+                else if (EventScript.ComedicChange)
+                {
+                    Tip += 0;
+                    //   CustomerBox.GetComponentInChildren<Text>().text = 
+                    // Changes Customers text to a negative responce at this array
+                    // Progress is added by 3 to continue through the list
+                    TestText.text = CustomerDialogueComedy[0 + Progress];
+                }
                 else
                 {
                     // Changes customers text to a nutural response at this array
@@ -57,6 +66,14 @@ public class DialogueManager : MonoBehaviour {
                     TestText.text = CustomerDialogueNegative[1 + Progress];
 
                 }
+                else if (EventScript.ComedicChange)
+                {
+                    Tip += 0;
+                    //   CustomerBox.GetComponentInChildren<Text>().text = 
+                    // Changes Customers text to a negative responce at this array
+                    // Progress is added by 3 to continue through the list
+                    TestText.text = CustomerDialogueComedy[0 + Progress];
+                }
                 else
                 {  // Changes customers text to a nutural response at this array
                     // Progress is added by 3 to continue through the list
@@ -74,6 +91,14 @@ public class DialogueManager : MonoBehaviour {
                     TestText.text = CustomerDialogueNegative[2 + Progress];
 
                 }
+                else if (EventScript.ComedicChange)
+                {
+                    Tip += 0;
+                    //   CustomerBox.GetComponentInChildren<Text>().text = 
+                    // Changes Customers text to a negative responce at this array
+                    // Progress is added by 3 to continue through the list
+                    TestText.text = CustomerDialogueComedy[0 + Progress];
+                }
                 else
                 {   
                     // Changes customers text to a nutural response at this array
@@ -88,8 +113,18 @@ public class DialogueManager : MonoBehaviour {
     
         Events[EventSteps].SetActive(false);
         EventSteps += 1;
-        Events[EventSteps].SetActive(true);
-        Val += 1;
+        if (Val < 4)
+        {
+            Events[EventSteps].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("NO MORE");
+            // Go to next scene or store
+            // make player jump out
+            // make player leave
+        }
+            Val += 1;
         // Adds by 3 to continue the array
         Progress += 3;
 
