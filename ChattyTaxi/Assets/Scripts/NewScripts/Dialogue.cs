dce0695f97517ea94746a2bb5dfc6924176cec95
Gabriel P <gaberofl42@gmail.com>
2019-04-02 09:52:09 +1100

    Currency
    Added currency system to game

--- ChattyTaxi/Assets/Scripts/NewScripts/Dialogue.cs
+++ ChattyTaxi/Assets/Scripts/NewScripts/Dialogue.cs
@@ -10,7 +10,18 @@ public class Dialogue : MonoBehaviour
     public Text Text2;
     public Text Text3;
     public Text CustomerText;
- 
+  
+    // Tips for actions
+    // 0 is start of convo 
+    // 1,2,3 are the tip you get for each response
+    // EG reponse 1 gives you 50C while repsonse 2 gives 10C  
+
+    public float[] GoodTips;
+    public float[] BadTips;
+    public float[] NeutralTips;
+
+
+
     public Button GoodButton;
     public Button NeutralButton;
     public Button BadButton;
@@ -21,8 +32,9 @@ public class Dialogue : MonoBehaviour
     public string[] CustomerResponse;
 
     private GameObject ConvoManagerGameObj;
-    ConvoManager ConvoManagerScript;
-
+    private GameObject StoreGameObj;
+    private StoreScript Store;
+    private ConvoManager ConvoManagerScript;
     private bool Good;
     private bool Bad;
     private bool Neutral;
@@ -30,7 +42,9 @@ public class Dialogue : MonoBehaviour
     void Start()
     {
         ConvoManagerGameObj = GameObject.FindGameObjectWithTag("ConvoManager");
-           ConvoManagerScript = ConvoManagerGameObj.GetComponent<ConvoManager>();
+        ConvoManagerScript = ConvoManagerGameObj.GetComponent<ConvoManager>();
+        StoreGameObj = GameObject.FindGameObjectWithTag("Store");
+        Store = StoreGameObj.GetComponent<StoreScript>();
     }
 
  
@@ -43,23 +57,27 @@ public class Dialogue : MonoBehaviour
             case 1:
                 if (Good)
                 {
+                    Store.Tip += GoodTips[1];
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
                 }
                 else if (Neutral)
                 {
+                    Store.Tip += NeutralTips[1];
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
 
                 }
                 else if (Bad)
                 {
+                    Store.Tip += BadTips[1];
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
 
                 }
                 else
                 {
+                    Store.Tip += GoodTips[0];
                     Text1.text = GoodResponse[0];
                     Text2.text = GoodResponse[1];
                     Text3.text = GoodResponse[2];
@@ -72,22 +90,27 @@ public class Dialogue : MonoBehaviour
                 {
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
+                    Store.Tip += NeutralTips[2];
 
                 }
                 else if(Good)
                 {
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
+                    Store.Tip += GoodTips[2];
 
                 }
                 else if (Bad)
                 {
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
+                    Store.Tip += BadTips[2];
 
                 }
                 else
                 {
+                    Store.Tip += NeutralTips[0];
+
                     Text1.text = NeuturalResponse[0];
                     Text2.text = NeuturalResponse[1];
                     Text3.text = NeuturalResponse[2];
@@ -101,22 +124,27 @@ public class Dialogue : MonoBehaviour
                 {
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
+                    Store.Tip += BadTips[3];
 
                 }
                 else if (Good)
                 {
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
+                    Store.Tip += GoodTips[3];
 
                 }
                 else if (Neutral)
                 {
                     Debug.Log("GET TIP CUSTOMER LEAVES");
                     ConvoManagerScript.Events();
+                    Store.Tip += NeutralTips[3];
 
                 }
                 else
                 {
+                    Store.Tip += BadTips[0];
+
                     Text1.text = BadResponse[0];
                     Text2.text = BadResponse[1];
                     Text3.text = BadResponse[2];
